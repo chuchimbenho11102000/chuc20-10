b4aaa3b875a7ba696d21bf27098162540750ef7b
chuchimbenho11102000 <91276736+chuchimbenho11102000@users.noreply.github.com>
2022-10-21 16:22:19 +0700

    ok

---
+++ tang-nguoi-yeu-hihi-main/config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+  introTitle: "Hello người đẹp!",
+  introDesc: `Trái đất vốn dĩ rất tròn\n
+    Anh thắng đẹp thế em còn không yêu\n
+    Thế em phải biết một điều\n
+    Trai ngoan tốt bụng đâu nhiều như anh :))`,
+  btnIntro: "hihi",
+  title: "Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙",
+  desc: "Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ",
+  btnYes: "Vẫn cứ là iu anh <33",
+  btnNo: "Không, anh bị đin à >.<",
+  question: "Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3",
+  btnReply: "Gửi cho anh <3",
+  reply: "Yêu thì yêu mà không yêu thì yêu <33333333",
+  mess: "Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘",
+  messDesc: "Đi cùng anh đến tận cùng thời gian nhé babiii :3",
+  btnAccept: "Okiiiii lun <3",
+  messLink: "https://www.facebook.com/tieukhongtuoc", //link mess của các bạn. VD: https://m.me/nam.nodemy
+};
